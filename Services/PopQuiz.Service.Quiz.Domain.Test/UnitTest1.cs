333b8b07eb647700e4e915fd3de4f228bc469a06
Jay <jaydg@pobox.com>
2018-11-10 07:36:16 -0700

    Rework initial solution layout focusing on services first.

---
+++ Services/PopQuiz.Service.Quiz.Domain.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace PopQuiz.Service.Domain.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
