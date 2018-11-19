54a35e3827316b399f5806c52b0edef06bd0d8f4
Jay <jaydg@pobox.com>
2018-11-19 05:29:28 -0700

    Add initial SPA application skeleton.

---
+++ Web/PopQuizSPA/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'popquiz-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'PopQuizSPA';
+}
